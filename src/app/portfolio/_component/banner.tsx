8a4106293efbe4127b4bcca4a6576e9ce9edd2c2
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 03:20:31 +0600

    style: Refactor Banner component layout for improved structure and responsiveness

--- src/app/portfolio/_component/banner.tsx
+++ src/app/portfolio/_component/banner.tsx
@@ -1,21 +1,36 @@
 import { StyledWord } from "@/components";
 import Heading from "@/components/heading";
+import { facebookIcon, Icon, instagramIcon, xIcon } from "@/components/icons";
 import { Container, Section } from "@/components/layout";
 
 export default function Banner() {
   return (
     <Section className="h-screen [background-image:url(/images/hero-pattern.png)] bg-cover">
-      <Container size="md">
-        <Heading as="h1" size="h1">
-          Trusted&nbsp;<StyledWord>Partner</StyledWord>&nbsp;for Your
-          Website&nbsp;<StyledWord>Develop.</StyledWord>
-        </Heading>
-        <div className="flex">
-          <div className="basis-96 shrink-0"></div>
-          <p>
-            Building the worlds best marketing websites for over a decade. Your
-            trusted partner for strategy, design, and dev.
-          </p>
+      <Container>
+        <div className="max-w-5.5xl">
+          <Heading as="h1" size="h1">
+            Trusted&nbsp;<StyledWord>Partner</StyledWord>&nbsp;for Your
+            Website&nbsp;<StyledWord>Develop.</StyledWord>
+          </Heading>
+          <div className="flex flex-col lg:flex-row gap-10 mt-14">
+            <div className="lg:order-2 lg:flex-1">
+              <p>
+                Building the worlds best marketing websites for over a decade.
+                Your trusted partner for strategy, design, and dev.
+              </p>
+            </div>
+            <div className="lg:order-1 lg:basis-96 shrink">
+              <div className="relative inline-block lg:rotate-[-90deg] lg:origin-[50%_100%]">
+                @williamrey
+                <div className="flex gap-5 items-center mt-4">
+                  <span className="w-12 h-0.5 bg-grey-900 inline-block"></span>
+                  <Icon src={facebookIcon} alt="Facebook" />
+                  <Icon src={instagramIcon} alt="Instagram" />
+                  <Icon src={xIcon} alt="X" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </Section>
