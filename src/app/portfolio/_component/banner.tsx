6512934b08d6a8f49092e9689e83cb18c2436c67
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 02:19:35 +0600

    style: Update Banner component layout for improved structure and responsiveness

--- src/app/portfolio/_component/banner.tsx
+++ src/app/portfolio/_component/banner.tsx
@@ -5,12 +5,17 @@ import { Container, Section } from "@/components/layout";
 export default function Banner() {
   return (
     <Section className="h-screen [background-image:url(/images/hero-pattern.png)] bg-cover">
-      <Container>
-        <div className="max-w-6xl">
-          <Heading as="h1" size="h1">
-            Trusted <StyledWord>Partner</StyledWord> for Your Website{" "}
-            <StyledWord>Develop.</StyledWord>
-          </Heading>
+      <Container size="md">
+        <Heading as="h1" size="h1">
+          Trusted&nbsp;<StyledWord>Partner</StyledWord>&nbsp;for Your
+          Website&nbsp;<StyledWord>Develop.</StyledWord>
+        </Heading>
+        <div className="flex">
+          <div className="basis-96 shrink-0"></div>
+          <p>
+            Building the worlds best marketing websites for over a decade. Your
+            trusted partner for strategy, design, and dev.
+          </p>
         </div>
       </Container>
     </Section>
