71ac96808f68e50ef25c0ce2ac003399175bf810
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 14:03:54 +0600

    feat: Enhance Button component with icon support and update styling

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,28 +1,40 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
+import { Icon } from "./icons";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean;
   className?: string;
+  variant?: "primary";
+  icon?: React.ReactNode;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, asChild = false, ...props }, ref) => {
+  ({ className, children, variant, icon, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
 
     return (
       <Comp
         className={cn(
-          "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors",
-          "bg-primary text-white px-4 py-2 hover:bg-primary/90",
+          "cursor-pointer inline-flex items-center justify-center rounded-md text-lg font-medium transition-colors",
+          "text-white py-1.5",
           "disabled:opacity-50 disabled:pointer-events-none",
+          "rounded-full border border-grey-900",
+          variant === "primary" && "text-grey-900 pr-3",
           className
         )}
         ref={ref}
         {...props}
-      />
+      >
+        {icon && (
+          <span className="flex items-center justify-center rounded-full aspect-square border border-grey-900 size-16 mr-3">
+            {icon}
+          </span>
+        )}
+        {children}
+      </Comp>
     );
   }
 );
