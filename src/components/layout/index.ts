c0801c362009c039d0ece5f2a6b01b79b5d1c602
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 14:04:09 +0600

    feat: Export SectionHeader, SectionName, and SectionTitle from Section component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,9 @@
 export { default as Container } from "./container";
-export { default as Section } from "./section";
+export {
+  default as Section,
+  SectionHeader,
+  SectionName,
+  SectionTitle,
+} from "./section";
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
