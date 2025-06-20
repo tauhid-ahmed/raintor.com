367b4a2348cd0d51267c42a4cd4d395c7456f5a0
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 18:04:20 +0600

    feat: add footerData with contact, menu, social, and call information

---
+++ src/data/my-information.tsx
@@ -0,0 +1,22 @@
+export const footerData = [
+  {
+    key: "contact",
+    title: "Say hello",
+    items: ["hello@devlop.me.com", "mahbubul@me.com"],
+  },
+  {
+    key: "menu",
+    title: "Menu",
+    items: ["Home", "About", "Portfolio", "Blog"],
+  },
+  {
+    key: "social",
+    title: "Social",
+    items: ["Twitter", "Instagram", "Facebook", "Behance", "Dribbble"],
+  },
+  {
+    key: "call",
+    title: "Call",
+    items: ["+784549", "4981 00", "+8845 0100 211"],
+  },
+];
