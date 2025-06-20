10e8979163b227b1e906116dad9c6f7c6f69a733
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 14:04:50 +0600

    feat: Update PortfolioPage to include MySkills, AboutMe, and MyWorkProcess components

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,15 @@
+import AboutMe from "./_component/about-me";
 import Banner from "./_component/banner";
+import MySkills from "./_component/my-skills";
+import MyWorkProcess from "./_component/my-work-process";
 
 export default function PortfolioPage() {
   return (
     <>
       <Banner />
+      <MySkills />
+      <AboutMe />
+      <MyWorkProcess />
     </>
   );
 }
