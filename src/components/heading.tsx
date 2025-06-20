1ad8d5462d7a0ff5f1678f14121b32a458536fd0
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 14:04:25 +0600

    feat: Extend Heading component to support additional heading sizes and improve styling

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -2,15 +2,17 @@ import { cn } from "@/lib/utils";
 
 interface HeadingProps extends React.HtmlHTMLAttributes<HTMLHeadingElement> {
   as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  size: "h1" | "h2" | "h3" | "h4";
+  size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 // font size range h1 is larger...h4 is smaller
 const sizesClassNames = {
   h1: "text-h3 md:text-h2 lg:text-h1 tracking-tighter",
   h2: "text-h2",
-  h3: "text-h3",
+  h3: "text-3xl md:text-4xl lg:text-h3",
   h4: "text-h4",
+  h5: "text-xl",
+  h6: "text-base",
 };
 
 export default function Heading({
@@ -23,7 +25,7 @@ export default function Heading({
   return (
     <Comp
       className={cn(
-        "font-sans leading-relaxed",
+        "font-sans leading-relaxed text-foreground font-bold",
         sizesClassNames[size],
         className
       )}
@@ -32,5 +34,3 @@ export default function Heading({
     </Comp>
   );
 }
-
-// 64/64, 58/86, 78/126, 26/41
