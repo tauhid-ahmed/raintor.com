7c340298956a6378303a12f3c70838bba3b8d77b
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 14:03:44 +0600

    feat: Add downArrowIconBlack export and enhance RightArrowIcon component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export { default as xIcon } from "@/images/icons/x.svg";
 export { default as downArrowIcon } from "@/images/icons/down-arrow.svg";
+export { default as downArrowIconBlack } from "@/images/icons/down-arrow-black.svg";
 export { default as facebookIcon } from "@/images/icons/facebook.svg";
 export { default as instagramIcon } from "@/images/icons/instagram.svg";
 export { default as leftArrowIcon } from "@/images/icons/left-arrow.svg";
@@ -19,5 +20,25 @@ export function Icon({ src, alt, className }: IconProps) {
       src={src as string}
       alt={alt as string}
     />
+  );
+}
+
+export function RightArrowIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={37}
+      height={24}
+      viewBox="0 0 37 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M0 12h36m0 0c-3.943-.727-11.829-4.145-11.829-12M36 12c-3.943.727-11.829 4.145-11.829 12"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinejoin="round"
+      />
+    </svg>
   );
 }
