60acf2ceebec081fb2c264239f18e76d70e88a71
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-20 19:50:53 +0600

    feat: enhance skill icons with animation effects for improved visual appeal

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -11,21 +11,27 @@ export const skillsData: Skill[] = [
   {
     id: "1",
     title: "HTML & CSS",
-    icon: <ReactLogoIcon className="size-24" />,
+    icon: (
+      <ReactLogoIcon className="size-24 animate-spin [animation-duration:12s]" />
+    ),
     description:
       "Duis aute irure dolor in reprehenderit in voluptate. Ut enim ad minim veniam, quis",
   },
   {
     id: "2",
     title: "Javascript",
-    icon: <ReactLogoIcon className="size-24" />,
+    icon: (
+      <ReactLogoIcon className="size-24 animate-spin [animation-duration:22s] [animation-direction:reverse]" />
+    ),
     description:
       "Duis aute irure dolor in reprehenderit in voluptate. Ut enim ad minim veniam, quis",
   },
   {
     id: "3",
     title: "Webflow",
-    icon: <ReactLogoIcon className="size-24" />,
+    icon: (
+      <ReactLogoIcon className="size-24 animate-spin [animation-duration:18s]" />
+    ),
     description:
       "Duis aute irure dolor in reprehenderit in voluptate. Ut enim ad minim veniam, quis",
   },
